4ceca9c3eb78a99ef302ce0583aab125752e63cd
Sabarish V S <Sabarish_7@protonmail.com>
2025-05-02 23:39:22 +0530

    added upstash compatable redis client

--- backend/config/redisClient.js
+++ backend/config/redisClient.js
@@ -1,9 +1,27 @@
 const { createClient } = require("redis");
 
-const redisClient = createClient();
+let redisClient;
 
-redisClient.on("error", (err) => console.error("Redis Error:", err));
+async function connectRedis() {
+  if (redisClient) return redisClient;
 
-redisClient.connect();
+  redisClient = createClient({
+    url: process.env.UPSTASH_REDIS_REST_URL,
+    password: process.env.UPSTASH_REDIS_REST_TOKEN,
+  });
 
-module.exports = redisClient;
+  redisClient.on("error", (err) => {
+    console.error("Redis Client Error:", err);
+  });
+
+  await redisClient.connect();
+  console.log("✅ Connected to Upstash Redis");
+
+  return redisClient;
+}
+
+const redisClientPromise = connectRedis();
+
+module.exports = {
+  redisClientPromise,
+};
